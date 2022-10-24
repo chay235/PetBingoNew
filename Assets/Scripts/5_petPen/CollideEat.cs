ddd377c79c8d1d8e5bdafc4fe091edfddaf84587
Charley <005870952@coyote.csusb.edu>
2022-10-23 22:03:44 -0700

    my copy
    This is my copy PetBingoNew_5.

---
+++ Assets/Scripts/5_petPen/CollideEat.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollideEat : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GetComponentInChildren<Animator>().enabled = true;
+        Debug.Log("Trigger Detected");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GetComponentInChildren<Animator>().enabled = true;
+        Debug.Log("Hit Detected");
+    }
+
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
